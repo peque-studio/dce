aee84ab1634c526cfab53e8ad640b6598ed31127
Mike Redko <some.random.dev.mike@gmail.com>
2022-09-28 23:42:46 +0300

    [DCm:impr] DCM__V_FOR -> DCM__T_FOR

--- dcore/math/vector.h
+++ dcore/math/vector.h
@@ -6,7 +6,7 @@
 #define DCM__V3_DEF(N, T, _) typedef T DCmVector3##N[3]
 #define DCM__V4_DEF(N, T, _) typedef T DCmVector4##N[4]
 
-#define DCM__V_FOR(O, SEP, ...) \
+#define DCM__T_FOR(O, SEP, ...) \
     O(u8, uint8_t, __VA_ARGS__) SEP \
     O(u16, uint16_t, __VA_ARGS__) SEP \
     O(u32, uint32_t, __VA_ARGS__) SEP \
@@ -21,9 +21,9 @@
     O(f, float, __VA_ARGS__) SEP \
     O(d, double, __VA_ARGS__)
 
-DCM__V_FOR(DCM__V2_DEF, ;);
-DCM__V_FOR(DCM__V3_DEF, ;);
-DCM__V_FOR(DCM__V4_DEF, ;);
+DCM__T_FOR(DCM__V2_DEF, ;);
+DCM__T_FOR(DCM__V3_DEF, ;);
+DCM__T_FOR(DCM__V4_DEF, ;);
 
 typedef DCmVector2f DCmVector2;
 typedef DCmVector2i32 DCmOffset2;
@@ -59,16 +59,25 @@ typedef DCmVector4u32 DCmExtent4;
                                                       DCmVector##K##S vector, \
                                                       T scalar) { BODY }
 
-DCM__V_FOR(DCM__VV_F,, Addv, 2, DCM__V2_OP_FOR(DCM__VV_OP, ;, +=););
-DCM__V_FOR(DCM__VV_F,, Subv, 2, DCM__V2_OP_FOR(DCM__VV_OP, ;, -=););
-DCM__V_FOR(DCM__VV_F,, Mulv, 2, DCM__V2_OP_FOR(DCM__VV_OP, ;, *=););
-DCM__V_FOR(DCM__VV_F,, Divv, 2, DCM__V2_OP_FOR(DCM__VV_OP, ;, /=););
-DCM__V_FOR(DCM__VS_F,, Adds, 2, DCM__V2_OP_FOR(DCM__VS_OP, ;, +=););
-DCM__V_FOR(DCM__VS_F,, Subs, 2, DCM__V2_OP_FOR(DCM__VS_OP, ;, -=););
-DCM__V_FOR(DCM__VS_F,, Muls, 2, DCM__V2_OP_FOR(DCM__VS_OP, ;, *=););
-DCM__V_FOR(DCM__VS_F,, Divs, 2, DCM__V2_OP_FOR(DCM__VS_OP, ;, /=););
-DCM__V_FOR(DCM__SVV_F,, Dot, 2, return a[0] * b[0] + a[1] * b[1];);
-DCM__V_FOR(DCM__VV_F,, Cross, 3,
+DCM__T_FOR(DCM__VV_F,, Addv, 2, DCM__V2_OP_FOR(DCM__VV_OP, ;, +=););
+DCM__T_FOR(DCM__VV_F,, Subv, 2, DCM__V2_OP_FOR(DCM__VV_OP, ;, -=););
+DCM__T_FOR(DCM__VV_F,, Mulv, 2, DCM__V2_OP_FOR(DCM__VV_OP, ;, *=););
+DCM__T_FOR(DCM__VV_F,, Divv, 2, DCM__V2_OP_FOR(DCM__VV_OP, ;, /=););
+DCM__T_FOR(DCM__VS_F,, Adds, 2, DCM__V2_OP_FOR(DCM__VS_OP, ;, +=););
+DCM__T_FOR(DCM__VS_F,, Subs, 2, DCM__V2_OP_FOR(DCM__VS_OP, ;, -=););
+DCM__T_FOR(DCM__VS_F,, Muls, 2, DCM__V2_OP_FOR(DCM__VS_OP, ;, *=););
+DCM__T_FOR(DCM__VS_F,, Divs, 2, DCM__V2_OP_FOR(DCM__VS_OP, ;, /=););
+DCM__T_FOR(DCM__SVV_F,, Dot, 2, return a[0] * b[0] + a[1] * b[1];);
+DCM__T_FOR(DCM__VV_F,, Addv, 3, DCM__V3_OP_FOR(DCM__VV_OP, ;, +=););
+DCM__T_FOR(DCM__VV_F,, Subv, 3, DCM__V3_OP_FOR(DCM__VV_OP, ;, -=););
+DCM__T_FOR(DCM__VV_F,, Mulv, 3, DCM__V3_OP_FOR(DCM__VV_OP, ;, *=););
+DCM__T_FOR(DCM__VV_F,, Divv, 3, DCM__V3_OP_FOR(DCM__VV_OP, ;, /=););
+DCM__T_FOR(DCM__VS_F,, Adds, 3, DCM__V3_OP_FOR(DCM__VS_OP, ;, +=););
+DCM__T_FOR(DCM__VS_F,, Subs, 3, DCM__V3_OP_FOR(DCM__VS_OP, ;, -=););
+DCM__T_FOR(DCM__VS_F,, Muls, 3, DCM__V3_OP_FOR(DCM__VS_OP, ;, *=););
+DCM__T_FOR(DCM__VS_F,, Divs, 3, DCM__V3_OP_FOR(DCM__VS_OP, ;, /=););
+DCM__T_FOR(DCM__SVV_F,, Dot, 3, return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];);
+DCM__T_FOR(DCM__VV_F,, Cross, 3,
     dst[0] = dst[1] * src[2] - dst[2] * src[1];
     dst[1] = dst[2] * src[0] - dst[0] * src[2];
     dst[2] = dst[0] * src[1] - dst[1] * src[0];
